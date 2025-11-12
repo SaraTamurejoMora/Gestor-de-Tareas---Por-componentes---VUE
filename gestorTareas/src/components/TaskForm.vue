57bf19c962b37e78d236fc0dfae8b489fc1e4dc4
Sara <sara.tamurejo.7e8@itb.cat>
2025-11-12 19:40:59 +0000

    falta dividir

---
+++ gestorTareas/src/components/TaskForm.vue
@@ -0,0 +1,14 @@
+<template>
+
+    
+    <input v-model="nombreTarea" placeholder="Nombre de la tarea"></input>
+    <button @click="añadirTarea" class="añadir">Añadir</button>
+</template>
+
+
+
+
+
+
+<script setup>
+</script>
